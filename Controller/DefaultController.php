ebe962b5a77e4b3107b2613818cdda653b003ea6
Daniel Ribeiro <drgomesp@gmail.com>
2016-03-21 18:54:08 +0400

    First commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SymfonyDocker\DockerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SymfonyDockerBundle:Default:index.html.twig');
+    }
+}
---
+++ SymfonyDockerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SymfonyDocker\DockerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymfonyDockerBundle extends Bundle
+{
+}
