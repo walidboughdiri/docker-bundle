7418ed8a02cda46433ae5b5ffbf2587d075e6220
Daniel Ribeiro <drgomesp@gmail.com>
2016-03-21 19:50:08 +0400

    Basic functionallity working with build and up commands

---
+++ Command/DockerComposeBuildCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SymfonyDocker\DockerBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+/**
+ * Class DockerComposeBuildCommand
+ *
+ * @author Daniel Ribeiro <drgomesp@gmail.com>
+ * @package SymfonyDocker\DockerBundle\Command
+ */
+class DockerComposeBuildCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('docker:compose:build')
+            ->setDescription('Builds all containers with Docker Compose')
+            ->setHelp(<<<'EOF'
+The <info>%command.name%</info> command clears the application cache for a given environment
+and debug mode:
+
+  <info>php %command.full_name% --env=dev</info>
+  <info>php %command.full_name% --env=prod --no-debug</info>
+EOF
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Kernel $kernel */
+        $kernel = $this->getContainer()->get('kernel');
+        $dockerComposeFile = $kernel->locateResource('@SymfonyDockerBundle/Resources/config/docker/docker-compose.yml');
+
+        $output->writeln(shell_exec("docker-compose -f {$dockerComposeFile} build"));
+    }
+}
